6563bce9e5d583f7787fd30882a1c98daefd5b5f
Joe Maghzal <joemaghzal@icloud.com>
2023-06-10 12:47:27 +0300

    Updates

--- Sources/NetworkUI/Routes/Iterable.swift
+++ Sources/NetworkUI/Routes/Iterable.swift
@@ -8,24 +8,18 @@
 import Foundation
 
 public protocol Iterable: URLable {
-    var nonNilProperties: [String: CustomStringConvertible] {get}
     var parameters: [URLQueryItem] {get}
 }
 
 public extension Iterable {
-    var nonNilProperties: [String: CustomStringConvertible] {
+    var parameters: [URLQueryItem] {
         let mirror = Mirror(reflecting: self)
         guard let style = mirror.displayStyle, style == .struct || style == .class else {
-            return [:]
+            return []
         }
-        return mirror.children.reduce(into: [:]) { partialResult, item in
+        return mirror.children.reduce(into: [URLQueryItem]()) { partialResult, item in
             guard case Optional<CustomStringConvertible>.some = item.value, let property = item.label else {return}
-            partialResult[property] = item.value as? CustomStringConvertible
-        }
-    }
-    var parameters: [URLQueryItem] {
-        return nonNilProperties.reduce(into: [URLQueryItem]()) { partialResult, item in
-            partialResult.append(URLQueryItem(name: item.key, value: item.value.description))
+            partialResult.append(URLQueryItem(name: property, value: (item.value as? CustomStringConvertible)?.description))
         }
     }
 }
